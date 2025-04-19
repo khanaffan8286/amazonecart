12d0f0f0c806f72acde4f8d871b742b8c5bdfab0
khanaffan8286 <khanaffan82861@gamil.com>
2025-04-19 15:08:14 +0530

    chnges

---
+++ javascript/amazon.js
@@ -0,0 +1,89 @@
+const product = [
+    {
+        image : 'images/products/athletic-cotton-socks-6-pairs.jpg',
+        name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+        rating  :{
+            stars : 4.5,
+            count : 256
+        },
+        price : 200
+    },
+    {
+        image : '',
+        name : '',
+        rating : {
+            stars : 4.5,
+            count : 234
+        },
+        price : 232
+    },
+    {
+        image : '',
+        name : '',
+        rating : {
+            stars : 4.5,
+            count : 321
+        },
+        price : 211
+    }
+];
+
+let producthtml = '';
+
+product.forEach((products) => {
+    producthtml = producthtml + `
+        <div class="product-container">
+            <div class="product-image-container">
+                <img class="product-image"
+                src="${products.image}">
+            </div>
+
+            <div class="product-name limit-text-to-2-lines">
+                ${products.name}
+            </div>
+
+            <div class="product-rating-container">
+                <img class="product-rating-stars"
+                src="images/ratings/rating-${products.rating.stars * 10}.png">
+                <div class="product-rating-count link-primary">
+                ${products.rating.count}
+                </div>
+            </div>
+
+            <div class="product-price">
+                ₹${products.price}
+            </div>
+
+            <div class="product-quantity-container">
+                <select>
+                <option selected value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                </select>
+            </div>
+
+            <div class="product-spacer"></div>
+
+            <div class="added-to-cart">
+                <img src="images/icons/checkmark.png">
+                Added
+            </div>
+
+            <button class="add-to-cart-button button-primary">
+                Add to Cart
+            </button>
+        </div>
+    `;
+  
+});
+
+
+
+document.querySelector('.js-container-grid').innerHTML = producthtml;
