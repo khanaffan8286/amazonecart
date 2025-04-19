d2c1ade9816985703093196acb1558c5a137e714
khanaffan8286 <khanaffan82861@gamil.com>
2025-04-19 17:16:55 +0530

    c

--- data/products.js
+++ data/products.js
@@ -5,7 +5,7 @@ export const products = [
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
     rating: {
       stars: 4.5,
-      count: 87
+      count: 87  
     },
     price: 1090,
     keywords: [
